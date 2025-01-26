5bb2f6d77c9f922fc832df39bdecea396aa10bb0
YuKyi67 <chankhine91@gmail.com>
2025-01-27 03:41:08 +0630

    changed about text

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,24 +1,12 @@
 <x-guest-layout>
     <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" class="w-full" />
     <article class="prose mx-auto">
-        <h1>Ini the beninging...</h1>
+        <h1>Metro Tomyam Shop</h1>
         <p>
-            Tom Tom was founded in 1954 in a small wet market alleyway of Bangkok Chinatown.
+            Established in 2015, Metro Tomyam Shop has been a local favorite in Taman Melawati, offering premium flavors from a humble roadside setting. Our shop may look like your typical neighborhood stall, but the food we serve is anything but ordinary. Our signature tomyam, crafted with fresh, high-quality ingredients and bold, aromatic spices, delivers an upscale dining experience without the upscale price tag.
         </p>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo aperiam eos possimus omnis amet sed,
-            perferendis atque? Fugiat commodi, quia nemo tempore officiis cupiditate incidunt numquam animi accusamus
-            sint.
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo aperiam eos possimus omnis amet sed,
-            perferendis atque? Fugiat commodi, quia nemo tempore officiis cupiditate incidunt numquam animi accusamus
-            sint.
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo aperiam eos possimus omnis amet sed,
-            perferendis atque? Fugiat commodi, quia nemo tempore officiis cupiditate incidunt numquam animi accusamus
-            sint.
+            What truly sets us apart is our head chef, a skilled culinary expert from Thailand, the home of world-renowned cuisine. With a passion for authentic Thai flavors and years of experience, our chef brings the heart and soul of Thai cooking straight to your plate. At Metro Tomyam Shop, we’re dedicated to serving extraordinary food in a welcoming, down-to-earth atmosphere that keeps customers coming back for more. If you’re in Melawati, stop by and taste the difference for yourself!
         </p>
     </article>
 </x-guest-layout>
