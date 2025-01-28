3ef1cb5b9692b5466d0b00e84b8863d3b5e20b84
YuKyi67 <chankhine91@gmail.com>
2025-01-28 18:37:12 +0630

    add menu tab and images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,22 @@
 <x-guest-layout>
-    <img 
-        class="w-full h-[750px]"
-        src="{{ Vite::asset('resources/images/tomyum-temp-photo.jpg') }}"
-        alt="" 
-        srcset=""
-    >
+    <img class="w-full h-[750px]" src="{{ Vite::asset('resources/images/tomyum-temp-photo.jpg') }}" alt="" srcset="">
 
     <div class="container mx-auto px-4">
-        <div class="prose prose-headings:text-primary pb-8">
-            <h1>Today's special</h1>
-            {{-- <hr class="bg-primary" /> --}}
+        <div class="prose prose-headings:text-primary prose-a:no-underline pb-8">
+            <h1>What We Offer</h1>
+            {{--
+            <hr class="bg-primary" /> --}}
+            <div role="tablist" class="tabs tabs-bordered">
+                <a role="tab" class="tab">Tomyam Soup</a>
+                <a role="tab" class="tab tab-active">Noodles</a>
+                <a role="tab" class="tab">Rice Dishes</a>
+                <a role="tab" class="tab">Side Dishes</a>
+                <a role="tab" class="tab">Drinks</a>
+            </div>
         </div>
+
         
+
         <div class="grid md:grid-cols-4 gap-8">
             <x-card>
                 <h2 class="card-title">Shoes!</h2>
