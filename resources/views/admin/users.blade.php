30fe37c85583e27d04b11f1af208eb4cbcd1e769
YuKyi67 <chankhine91@gmail.com>
2025-01-29 23:24:39 +0630

    added user table

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,14 +1,38 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Users') }}
         </h2>
     </x-slot>
 
     <x-content>
-        @foreach ($users as $user)
-            {{ $user->name }}
-            {{ $user->role }}
-        @endforeach
+        <div class="overflow-x-auto">
+            <table class="table">
+                <!-- head -->
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    @foreach ($users as $user)
+                        <!-- heighlight on hover -->
+                        <tr class="hover">
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->role }}</td>
+                            <th>
+                                <button class="btn btn-ghost btn-xs">Edit</button>
+                                <button class="btn btn-warning btn-xs">Delete</button>
+                            </th>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
     </x-content>
 </x-app-layout>
