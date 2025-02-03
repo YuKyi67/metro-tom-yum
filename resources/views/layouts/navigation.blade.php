62c297670696020738d239f8f9f7c6d5029b82d5
YuKyi67 <chankhine91@gmail.com>
2025-02-03 20:51:16 +0630

    add action checked

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="/">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
                 </div>
 
                 <!-- Navigation Links -->
--- resources/views/menu/items.blade.php
+++ resources/views/menu/items.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <x-content>
-        <div class="flex justify-end mb-8">
-            <a type="button" class="btn btn-primary" href="{{ route('menu.create') }}">+ Add New Item</a>
-        </div>
+        @if (Auth::user()->role === 'admin')
+            <div class="flex justify-end mb-8">
+                <a type="button" class="btn btn-primary" href="{{ route('menu.create') }}">+ Add New Item</a>
+            </div>
+        @endif
         @include('menu.partials.menu-item-list')
     </x-content>
 </x-app-layout>
