80d726394c0eaabb87d1a0b9ac5e091b9c04310e
YuKyi67 <chankhine91@gmail.com>
2025-02-04 18:15:41 +0630

    bypass current password request if user is admin

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
@@ -13,14 +15,15 @@ class PasswordController extends Controller
     /**
      * Update the user's password.
      */
-    public function update(Request $request): RedirectResponse
+    public function update(Request $request, User $user): RedirectResponse
     {
+        // skip require current password if user is admin
         $validated = $request->validateWithBag('updatePassword', [
-            'current_password' => ['required', 'current_password'],
+            'current_password' => [Auth::user()->role !== 'admin' ? 'required' : 'nullable', 'current_password'],
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
-        $request->user()->update([
+        $user->update(attributes: [
             'password' => Hash::make($validated['password']),
         ]);
 
